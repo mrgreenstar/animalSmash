149598293d8bee9756260c74a4bef169dc871d9a
mrgreenstar <andrey.lalaev@gmail.com>
2018-11-19 23:43:21 +0300

    nothing new

--- main/views.py
+++ main/views.py
@@ -9,13 +9,16 @@ from .models import Animal
 
 class IndexView(generic.View):
     template_name = 'main/index.html'
-    firstAnimal = random.choice(Animal.objects.all())
-    secondAnimal = random.choice(Animal.objects.all())
+
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'firstAnimal':self.firstAnimal.animal,
-            'secondAnimal':self.secondAnimal.animal})
+        firstAnimal = random.choice(Animal.objects.all())
+        secondAnimal = random.choice(Animal.objects.all())
+        return render(request, self.template_name, {'firstAnimal':firstAnimal,
+            'secondAnimal':secondAnimal})
 
     def post(self, request, *args, **kwargs):
-        logging.debug(request, args, kwargs)
-        return render(request, self.template_name, {'firstAnimal':self.firstAnimal.animal,
-            'secondAnimal':self.secondAnimal.animal})
+        firstAnimal = random.choice(Animal.objects.all())
+        secondAnimal = random.choice(Animal.objects.all())
+        logging.error(request.POST)
+        return render(request, self.template_name, {'firstAnimal':firstAnimal,
+            'secondAnimal':secondAnimal})
