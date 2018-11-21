ee872b9516e1257ea39bb69277015ab0bb7102cd
mrgreenstar <andrey.lalaev@gmail.com>
2018-11-21 08:41:18 +0300

    created a awful bugfix for rating system. Changed value for chosen animal in html. Now html value contains chosen&unchosen ids

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 import logging
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 
 from .models import Animal
@@ -15,29 +15,36 @@ class IndexView(generic.View):
     first_animal = random.choice(all_animals)
     all_animals.remove(first_animal)
     second_animal = random.choice(all_animals)
-    
+
     def get(self, request, *args, **kwargs):
+        logging.error("get")
+        all_animals = list(Animal.objects.all())
+        self.first_animal = random.choice(all_animals)
+        all_animals.remove(self.first_animal)
+        self.second_animal = random.choice(all_animals)   
         return render(request, self.template_name, {'first_animal':self.first_animal,
             'second_animal':self.second_animal})
 
     def post(self, request, *args, **kwargs):
-        if str(self.first_animal.id) == request.POST.get('first_animal'):
-            logging.error('first')
-            chosen_animal = self.first_animal
-            unchosen_animal = self.second_animal
+        logging.error(request.POST.items)
+        if request.POST.get("first_animal"):
+            chosen_animal_id = request.POST.get("first_animal").split('&')[0]
+            unchosen_animal_id = request.POST.get("first_animal").split('&')[1]
         else:
-            logging.error('second')
-            chosen_animal = self.second_animal
-            unchosen_animal = self.first_animal
+            chosen_animal_id = request.POST.get("second_animal").split('&')[0]
+            unchosen_animal_id = request.POST.get("second_animal").split('&')[1]
         
+        chosen_animal = Animal.objects.get(pk=chosen_animal_id)
+        unchosen_animal = Animal.objects.get(pk=unchosen_animal_id)
         chosen_animal.rating, unchosen_animal.rating = new_rating(chosen_animal.rating,
             unchosen_animal.rating)
         chosen_animal.save()
         unchosen_animal.save()
 
+        # For new animals after POST
         all_animals = list(Animal.objects.all())
         self.first_animal = random.choice(all_animals)
         all_animals.remove(self.first_animal)
-        self.second_animal = random.choice(all_animals)
+        self.second_animal = random.choice(all_animals)   
         return render(request, self.template_name, {'first_animal':self.first_animal,
             'second_animal':self.second_animal})
